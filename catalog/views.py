668157bf0a3c79d9d3d124b52c4cdb5a98e7a36f
PetrDidur <pdidur@yandex.ru>
2023-07-11 15:31:39 +0300

    fixed 404

--- catalog/views.py
+++ catalog/views.py
@@ -6,4 +6,9 @@ def index_homepage(request):
 
 
 def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(f'{name} {email} {message}')
     return render(request, 'main/contacts.html')
