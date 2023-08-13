616c9128bc5f451446106dd98daed231305814af
PetrDidur <pdidur@yandex.ru>
2023-08-13 15:06:03 +0300

    3rd fixes

--- catalog/forms.py
+++ catalog/forms.py
@@ -4,10 +4,28 @@ from catalog.models import Product, Version
 
 
 class StyleFormMixin:
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+
+    def init(self, args, **kwargs):
+        super().init(args, **kwargs)
         for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'form-control'
+
+            if isinstance(field.widget, forms.widgets.CheckboxInput):
+                field.widget.attrs['class'] = 'form-check-input'
+            elif isinstance(field.widget, forms.DateTimeInput):
+                field.widget.attrs['class'] = 'form-control flatpickr-basic'
+            elif isinstance(field.widget, forms.DateInput):
+                field.widget.attrs['class'] = 'form-control datepicker'
+            elif isinstance(field.widget, forms.TimeInput):
+                field.widget.attrs['class'] = 'form-control flatpickr-time'
+            elif isinstance(field.widget, forms.widgets.SelectMultiple):
+                field.widget.attrs['class'] = 'form-control select2 select2-multiple'
+            elif isinstance(field.widget, forms.widgets.Select):
+                field.widget.attrs['class'] = 'form-control select2'
+            elif isinstance(field.widget, forms.widgets.PasswordInput):
+                field.widget.attrs['class'] = 'form-control'
+
+            else:
+                field.widget.attrs['class'] = 'form-control'
 
 
 class ProductForm(StyleFormMixin, forms.ModelForm):
